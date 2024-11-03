950d7745f2ba421c8ee7fd0efba0c5e3348d497b
JericXK <jericxyrus.karganilla@lorma.edu>
2024-11-03 21:24:43 +0800

    Delete src directory

--- src/User.java
+++
@@ -1,9 +0,0 @@
-public class User {
-    public String first_name;
-    public String middle_name;
-    public String last_name;
-    public String address;
-    public String phone_number;
-    public String email;
-    public String gender;
-}
